76dd4b88100389b2791358aecbfba0d5ba77c86f
mwombeki6 <hackerlubex@protonmail.com>
2024-07-28 21:31:08 +0300

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Application, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import router from './routes/auth'
+import connectDB from './config/db'
 
-const app: Application = express()
+/*const app: Application = express()
 const port: number = 3000
 
 app.get('/', (req: Request, res: Response) => {
@@ -8,8 +11,21 @@ app.get('/', (req: Request, res: Response) => {
     res.render('index.html',)
 })
 
-app.post('')
+app.use('/api/auth', );
 
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`)
 })
+*/
+
+dotenv.config()
+
+const app = express()
+connectDB()
+
+app.use(express.json())
+app.use('/api/auth', router)
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
